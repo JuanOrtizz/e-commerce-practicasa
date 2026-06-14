657af7cad97174424927fcc5672f4ffb81a19c22
JuanOrtizz <juan_ignaortiz@outlook.com>
2026-06-14 01:43:14 -0300

    fix: Arreglo bug de credencial Email_Port para test en CI

--- project/project/settings.py
+++ project/project/settings.py
@@ -116,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 #Emails SMTP
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config("EMAIL_HOST", default="")
-EMAIL_PORT = config("EMAIL_PORT", default="", cast=int)
+EMAIL_PORT = config("EMAIL_PORT", default="587", cast=int)
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=False, cast=bool)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
