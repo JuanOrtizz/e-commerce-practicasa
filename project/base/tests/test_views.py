230bf806c7d157dd2a69d565db705a4083e82237
JuanOrtizz <juan_ignaortiz@outlook.com>
2026-06-14 01:35:53 -0300

    feat: Agrego test de ratelimit, servicio enviar_email y actualizo test_flujo_contacto_valido

--- project/base/tests/test_views.py
+++ project/base/tests/test_views.py
@@ -1,7 +1,8 @@
 import pytest
+from django.core import mail
+from django.test.utils import override_settings
 from django.urls import reverse
 from base.models import ConsultaModel
-
 
 @pytest.mark.django_db
 def test_index_status_200(client):
@@ -24,6 +25,7 @@ def test_flujo_contacto_invalido(client, invalid_consulta_data):
     assert ConsultaModel.objects.count() == 0
 
 
+@override_settings(EMAIL_BACKEND='django.core.mail.backends.locmem.EmailBackend')
 @pytest.mark.django_db
 def test_flujo_contacto_valido(client, consulta_data):
     response = client.post(reverse('contacto'), consulta_data)
@@ -31,3 +33,18 @@ def test_flujo_contacto_valido(client, consulta_data):
     assert data['success'] is True
     assert 'Recibimos tu consulta' in data['message']
     assert ConsultaModel.objects.count() == 1
+    assert len(mail.outbox) == 1
+    assert 'Consulta en la web' in mail.outbox[0].subject
+
+@override_settings(EMAIL_BACKEND='django.core.mail.backends.locmem.EmailBackend')
+@pytest.mark.django_db
+def test_ratelimit_contacto_bloquea(client, consulta_data):
+    from django.core.cache import cache
+    cache.clear()
+
+    for _ in range(5):
+        response = client.post(reverse('contacto'), consulta_data)
+        assert response.status_code == 200
+
+    response = client.post(reverse('contacto'), consulta_data)
+    assert response.status_code == 403
