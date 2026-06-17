7ca490f418f7d82b5a55f98626afd0c2947e505a
JuanOrtizz <juan_ignaortiz@outlook.com>
2026-06-17 13:07:27 -0300

    fix: Configuro ALLOWED_HOSTS y corrijo dj_database_url.parse() para Render (CD de Testing)

--- project/project/settings.py
+++ project/project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1", cast=Csv())
 
 # Application definition
 
@@ -81,7 +81,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 if config("DATABASE_URL", default=""):
     DATABASES = {
-        'default': dj_database_url.parse(config("DATABASE_URL", default=""))
+        'default': dj_database_url.parse(
+            config("DATABASE_URL"),
+            conn_max_age = 600,
+            ssl_require=True
+        )
     }
 else:
     DATABASES = {
