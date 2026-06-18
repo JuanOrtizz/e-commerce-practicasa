d8152192048c700c4ba6a97ebdff0de7a9500cdf
JuanOrtizz <juan_ignaortiz@outlook.com>
2026-06-18 13:16:30 -0300

    feat: Agrego modelos para app Productos, migracion y registro de modelos en Admin

--- project/productos/admin.py
+++ project/productos/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
+from .models import (
+    CategoriaModel, SubcategoriaModel, ColorModel, MedidaModel,
+    TagModel, ProductoModel, ProductoImagenModel
+)
 
-# Register your models here.
+
+@admin.register(CategoriaModel)
+class CategoriaModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nombre', 'slug']
+    search_fields = ['nombre']
+    prepopulated_fields = {'slug': ('nombre',)}
+
+
+@admin.register(SubcategoriaModel)
+class SubcategoriaModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nombre', 'categoria', 'slug']
+    search_fields = ['nombre']
+    list_filter = ['categoria']
+    prepopulated_fields = {'slug': ('nombre',)}
+
+
+@admin.register(ColorModel)
+class ColorModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nombre', 'codigo_hex']
+    search_fields = ['nombre']
+
+
+@admin.register(MedidaModel)
+class MedidaModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nombre']
+    search_fields = ['nombre']
+
+
+@admin.register(TagModel)
+class TagModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nombre']
+    list_filter = ['nombre']
+
+
+class ProductoImagenInline(admin.TabularInline):
+    model = ProductoImagenModel
+    extra = 1
+
+
+@admin.register(ProductoModel)
+class ProductoModelAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'nombre', 'sku', 'precio', 'stock',
+        'subcategoria', 'promocion', 'activo'
+    ]
+    list_filter = ['activo', 'subcategoria', 'promocion', 'tags']
+    search_fields = ['nombre', 'sku', 'descripcion']
+    prepopulated_fields = {'slug': ('nombre',)}
+    inlines = [ProductoImagenInline]
+    filter_horizontal = ['colores', 'medidas', 'tags']
