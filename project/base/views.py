47b353a3feba5b4398d834b5f754970ac925f11d
Luca-Enrique <lucaenr2005@gmail.com>
2026-06-18 20:05:54 -0300

    feat: Agrego el request para 'faqs'

--- project/base/views.py
+++ project/base/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'base/index.html')
+
+# View para FAQs
+def faqs(request):
+    return render(request, 'base/faqs.html')
