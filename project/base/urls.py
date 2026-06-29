3f05a4ab240427d2b8cc8c5bc7190acf2c0b752c
inaki <iakitrapiella27@hotmail.com>
2026-06-29 11:37:53 -0300

    feat: agrego templates, urls y views de legales e historia

--- project/base/urls.py
+++ project/base/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('cambios_y_devoluciones/', views.cambios_y_devoluciones, name='cambios_y_devoluciones'),
+    path('nuestra_historia/', views.nuestra_historia, name='nuestra_historia'),
+    path('politicas_de_privacidad/', views.politicas_de_privacidad, name='politicas_de_privacidad'),
+    path('terminos_y_condiciones/', views.terminos_y_condiciones, name='terminos_y_condiciones'),
+    path('cambios_y_devoluciones/', views.cambios_y_devoluciones, name='cambios_y_devoluciones'),
 ]
--- project/base/views.py
+++ project/base/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'base/index.html')
+
+def cambios_y_devoluciones(request):
+    return render(request, 'base/cambios_y_devoluciones.html')
+
+def nuestra_historia(request):
+    return render(request, 'base/nuestra_historia.html')
+
+def politicas_de_privacidad(request):
+    return render(request, 'base/politicas_de_privacidad.html')
+
+def terminos_y_condiciones(request):
+    return render(request, 'base/terminos_y_condiciones.html')
+
+def cambios_y_devoluciones(request):
+    return render(request, 'base/cambios_y_devoluciones.html')
