fd9fb9663b66d07b7fb52710e168e19d06662e5d
JuanOrtizz <juan_ignaortiz@outlook.com>
2026-08-13 13:00:38 -0300

    feat: agrego campo estado al modelo consultas (pendiente/resuelta)

--- project/base/models.py
+++ project/base/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 # Create your models here.
 class ConsultaModel(models.Model):
+    class Estados(models.TextChoices):
+        PENDIENTE = 'pendiente', 'Pendiente'
+        RESUELTA = 'resuelta', 'Resuelta'
+
     nombre = models.CharField(max_length=100)
     email = models.EmailField()
     telefono = models.CharField(max_length=20)
     mensaje = models.TextField()
     fecha_y_hora = models.DateTimeField(auto_now_add=True, db_index=True)
+    estado = models.CharField(max_length=20, choices=Estados.choices, default=Estados.PENDIENTE)
 
     def __str__(self):
         return self.nombre
